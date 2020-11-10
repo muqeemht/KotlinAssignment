2f8e91f6049ae36986502a4b1ad96755dfcf3794
Muqeem Ahmad <ahmad.muqeem@gmail.com>
2020-11-10 22:31:54 +0530

    Enabled Retrofit Caching

--- app/src/main/java/com/muqeem/assignment/base/network/retrofit/RestClient.kt
+++ app/src/main/java/com/muqeem/assignment/base/network/retrofit/RestClient.kt
@@ -6,7 +6,10 @@ import com.muqeem.assignment.BuildConfig.BASE_URL
 import com.muqeem.assignment.base.app.MyApplication
 import okhttp3.Cache
 import okhttp3.CacheControl
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
@@ -17,51 +20,23 @@ object RestClient {
 
     private fun retrofit(): Retrofit {
 
-        val  HEADER_CACHE_CONTROL: String = "Cache-Control";
-        val  HEADER_PRAGMA: String = "Pragma";
 
-        val httpCacheDirectory = File(MyApplication.getAppContext().getCacheDir(), "responses")
+
+
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val httpCacheDirectory = File(MyApplication.getAppContext().getCacheDir(), "http-cache")
         val cacheSize:Long = 10 * 1024 * 1024 // 10 MiB
 
         val cache = Cache(httpCacheDirectory, cacheSize)
 
         val builder = OkHttpClient.Builder().cache(cache).
+        addInterceptor(provideOfflineCacheInterceptor()).
 
-        addNetworkInterceptor { chain ->
-            var request = chain.request()
-            request = if (!CommonUtils.isConnectedToInternet()) {
-                val cacheControl = CacheControl.Builder()
-                    .maxStale(7, TimeUnit.DAYS)
-                    .build()
+        addNetworkInterceptor(networkCacheInterceptor).
+        addInterceptor(loggingInterceptor)
 
-                request.newBuilder().cacheControl(cacheControl)
-                    .build()
-            } else {
-                val cacheControl = CacheControl.Builder()
-                    .maxAge(5, TimeUnit.SECONDS)
-                    .build()
-
-                request.newBuilder().cacheControl(cacheControl)
-                    .build()
-            }
-            chain.proceed(request)
-        }.
-
-        addInterceptor { chain ->
-            var request = chain.request()
-            if(!CommonUtils.isConnectedToInternet()) {
-                val cacheControl = CacheControl.Builder()
-                    .maxStale(7, TimeUnit.DAYS)
-                    .build()
-
-                request = request.newBuilder().cacheControl(cacheControl)
-                    .removeHeader(HEADER_PRAGMA)
-                    .removeHeader(HEADER_CACHE_CONTROL)
-                    .header(HEADER_CACHE_CONTROL, cacheControl.toString())
-                    .build()
-            }
-            chain.proceed(request)
-        }
         val clientBuilder = builder.build()
         return Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
@@ -74,5 +49,55 @@ object RestClient {
     val retrofitService: ApiEndPoint by lazy {
         retrofit().create(ApiEndPoint::class.java)
     }
+
+    private val networkCacheInterceptor = Interceptor { chain ->
+
+
+        val  HEADER_CACHE_CONTROL: String = "Cache-Control";
+        val  HEADER_PRAGMA: String = "Pragma";
+
+        val response: Response = chain.proceed(chain.request())
+
+        val cacheControl: CacheControl
+
+        cacheControl = if (CommonUtils.isConnectedToInternet()) {
+            CacheControl.Builder()
+                    .maxAge(0, TimeUnit.SECONDS)
+                    .build()
+        } else {
+            CacheControl.Builder()
+                    .maxStale(7, TimeUnit.DAYS)
+                    .build()
+        }
+
+        response.newBuilder()
+                .removeHeader(HEADER_PRAGMA)
+                .removeHeader(HEADER_CACHE_CONTROL)
+                .header(HEADER_CACHE_CONTROL, cacheControl.toString())
+                .build()
+
+
+
+    }
+
+    private fun provideOfflineCacheInterceptor(): Interceptor? {
+        return Interceptor { chain ->
+            var request = chain.request()
+            val  HEADER_CACHE_CONTROL: String = "Cache-Control";
+            val  HEADER_PRAGMA: String = "Pragma";
+            if (!CommonUtils.isConnectedToInternet()) {
+                val cacheControl = CacheControl.Builder()
+                        .maxStale(7, TimeUnit.DAYS)
+                        .build()
+                request = request.newBuilder()
+                        .removeHeader(HEADER_PRAGMA)
+                        .removeHeader(HEADER_CACHE_CONTROL)
+                        .cacheControl(cacheControl)
+                        .build()
+            }
+            chain.proceed(request)
+        }
+    }
+
 
     }
